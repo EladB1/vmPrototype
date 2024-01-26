df6d135c721086a19f58dcbd74d3ffb7ea698ede
EladB1 <eb86@njit.edu>
2024-01-26 17:00:38 -0500

    Initial commit

---
+++ stackVM/bytecode.h
@@ -0,0 +1,20 @@
+typedef enum {
+    ADD_I32,
+    SUB_I32,
+    MUL_I32,
+    LT_I32,
+    EQ_I32,
+    JMP, // branch
+    JMPT, // branch if true
+    JMPF, // branch if false
+    CONST_I32,
+    LOAD, // load from local
+    GLOAD, // load from global
+    STORE, // store to local
+    GSTORE, // store to global
+    PRINT,
+    POP,
+    HALT,
+    CALL,
+    RET
+} opcode;
