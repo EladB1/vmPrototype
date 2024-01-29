f25e753471895fe0ca0f2930e57a1634f9c521fc
EladB1 <eb86@njit.edu>
2024-01-29 17:41:10 -0500

    Started to lay groundwork for supporting multiple types

--- stackVM/vm2.c
+++ stackVM/vm2.c
@@ -1,21 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 #include "stringvector.h"
 
 #define STACK_SIZE 100
+
+typedef enum {
+    Addr = 1,
+    Int,
+    Dbl,
+    Str,
+    Arr,
+    Bool
+} Type;
+
+typedef union memberVal {
+    int intVal;
+    double dblVal;
+    bool boolVal;
+    char* strVal;
+    union memberVal* arrayVal;
+} MemberValue;
+
+typedef struct {
+    Type type;
+    MemberValue value;
+    int size;
+} StackMember;
 
 typedef struct {
     int* locals;
     int* globals;
     StringVector* code;
-    int* stack;
+    //int* stack;
+    StackMember* stack;
     int pc;
     int sp;
     int fp;
     int lc;
     int gc;
 } VM;
+
+StackMember binaryOperation(StackMember lhs, StackMember rhs, char* operation) {
+
+}
+
+bool constantIsInteger(char* constant) {
+    for (int i = 0; i < strlen(constant); i++) {
+        if (!isdigit(constant[i]))
+            return false;
+    }
+    return true;
+}
+
+bool constantIsDouble(char* constant) {
+    int dotCount = 0;
+    for (int i = 0; i < strlen(constant); i++) {
+        if (constant[i] == '.')
+            dotCount++;
+        else if (!isdigit(constant[i]))
+            return false;
+    }
+    return dotCount == 1;
+}
+
+bool constantIsBoolean(char* constant) {
+    int len = strlen(constant);
+    return strncmp(constant, "0", len) == 0 || strncmp(constant, "1", len) == 0;
+}
 
 
 VM* init(StringVector* code, int datasize) {
@@ -39,11 +93,11 @@ void destroy(VM* vm) {
     free(vm);
 }
 
-void push(VM* vm, int value) {
+void push(VM* vm, StackMember value) {
     vm->stack[++vm->sp] = value;
 }
 
-int pop(VM* vm) {
+StackMember pop(VM* vm) {
     return vm->stack[vm->sp--];
 }
 
@@ -74,7 +128,9 @@ void display(VM* vm) {
 void run(VM* vm) {
     printf("Running program...\n");
     char* opcode;
-    int len, value, addr, lhs, rhs;
+    StackMember value, lhs, rhs, rval;
+    char* next;
+    int len, addr;
     //int offset, argc, rval;
     while (1) {
         opcode = getNext(vm);
@@ -84,7 +140,19 @@ void run(VM* vm) {
             return; // stop program
         }
         else if (strncmp(opcode, "LOAD_CONST", len) == 0) {
-            value = atoi(getNext(vm));
+            next = getNext(vm);
+            printf("Is double: %d\n", constantIsDouble(next));
+            if (constantIsInteger(next)) {
+                value.type = Int;
+                value.size = 1;
+                value.value.intVal = atoi(next);
+            }
+            else if (constantIsDouble(next)) {
+                value.type = Dbl;
+                value.size = 1;
+                value.value.dblVal = atof(next);
+            }
+            //value = atoi(getNext(vm));
             push(vm, value);
         }
         else if (strncmp(opcode, "DUP", len) == 0) {
@@ -96,13 +164,49 @@ void run(VM* vm) {
         else if (strncmp(opcode, "ADD", len) == 0) {
             rhs = pop(vm);
             lhs = pop(vm);
-            push(vm, lhs + rhs);
+            if (lhs.type == Dbl || rhs.type == Dbl) {
+                rval.type = Dbl;
+                if (lhs.type == rhs.type)
+                    rval.value.dblVal = lhs.value.dblVal + rhs.value.dblVal;
+                else if (lhs.type == Int)
+                    rval.value.dblVal = lhs.value.intVal + rhs.value.dblVal;
+                else
+                    rval.value.dblVal = lhs.value.dblVal + rhs.value.intVal;
+            }
+                
+            else {
+                rval.type = Int;
+                rval.value.intVal = lhs.value.intVal + rhs.value.intVal;
+            }
+            push(vm, rval);
+        }
+        /*else if (strncmp(opcode, "SUB", len) == 0) {
+            rhs = pop(vm);
+            lhs = pop(vm);
+            push(vm, lhs - rhs);
         }
         else if (strncmp(opcode, "MUL", len) == 0) {
             rhs = pop(vm);
             lhs = pop(vm);
             push(vm, lhs * rhs);
-
+        }
+        else if (strncmp(opcode, "DIV", len) == 0) {
+            rhs = pop(vm);
+            if (rhs == 0) {
+                printf("Division by zero error\n");
+                return;
+            }
+            lhs = pop(vm);
+            push(vm, lhs / rhs);
+        }
+        else if (strncmp(opcode, "REM", len) == 0) {
+            rhs = pop(vm);
+            if (rhs == 0) {
+                printf("Division by zero error\n");
+                return;
+            }
+            lhs = pop(vm);
+            push(vm, lhs % rhs);
         }
         else if (strncmp(opcode, "EQ", len) == 0) {
             rhs = pop(vm);
@@ -131,6 +235,10 @@ void run(VM* vm) {
             value = vm->globals[addr];
             push(vm, value);
         }
+        else if (strncmp(opcode, "JMP", len) == 0) {
+            addr = atoi(getNext(vm));
+            vm->pc = addr;
+        }
         else if (strncmp(opcode, "JMPT", len) == 0) {
             addr = atoi(getNext(vm));
             if (pop(vm))
@@ -152,7 +260,7 @@ void run(VM* vm) {
                 value = atoi(get(vm->code, vm->pc));
             }
             push(vm, value);
-        }
+        }*/
         else {
             printf("Unknown bytecode: %s\n", opcode);
             break;
