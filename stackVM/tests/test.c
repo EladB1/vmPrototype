a5551c12f6302d77bf31eadc9d75bf382db8e689
EladB1 <EladB1@users.noreply.github.com>
2024-02-24 18:51:17 -0500

    Setup use of criterion for writing unit tests

--- stackVM/tests/test.c
+++ stackVM/tests/test.c
@@ -1,23 +1,12 @@
-#include <stdio.h>
+#include <criterion/criterion.h>
 
-#include "../test_runner.h"
-#include "../dataconstant.h"
+#include "../src/dataconstant.h"
 
-bool testAdd_twoInts() {
-    DataConstant result = binaryArithmeticOperation(createInt(3), createInt(4), "+");
-    return isEqual(result, createInt(7));
+Test(sample, test) {
+    cr_expect(2 * 3 == 6);
 }
 
-bool testAdd_twoFloats() {
-    DataConstant result = binaryArithmeticOperation(createDouble(3.01), createDouble(4.59), "+");
-    return isEqual(result, createDouble(7.6));
-}
-
-int main(int argc, char** argv) {
-    UnitTest tests[] = {
-        {"testAdd(1)", testAdd_twoInts},
-        {"testAdd(2)", testAdd_twoFloats}
-    };
-    test_runner(tests, 2);
-    return 0;
+Test(DataConstant, dc_test1) {
+    DataConstant num = createInt(4);
+    cr_expect(num.value.intVal == 4);
 }
