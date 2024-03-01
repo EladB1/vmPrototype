a263c9012e36be5dc0d921421a5216c058a5bb43
Elad Bergrin <EladB1@users.noreply.github.com>
2024-03-01 21:36:46 +0000

    Added the ability to pass in bytecode file and improved filereader error message (#3)

--- stackVM/src/app.c
+++ stackVM/src/app.c
@@ -5,9 +5,30 @@
 #include "filereader.h"
 #include "vm.h"
 
+char* getUsage(char* prog_name) {
+    char* verbose = "\t-v, --verbose:\tDisplay the internal VM state at each execution cycle\n";
+    char* help = "\t-h, --help:\tShow this help message\n";
+    char* message = "";
+    asprintf(&message, "Usage: %s FILE [OPTIONS]\nOPTIONS:\n%s%s", prog_name, verbose, help);
+    return message;
+}
+
 int main(int argc, char** argv) {
-    bool verbose = argc >= 2 && (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--verbose") == 0);
-    SourceCode src = read_file("input.txt");
+    char filename[256];
+    bool verbose;
+    if (argc >= 2) {
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+            printf("%s\n", getUsage(argv[0]));
+            return 0;
+        }
+        strncpy(filename, argv[1], strlen(argv[1]) + 1);
+        verbose = (argc >= 3) && (strcmp(argv[2], "-v") == 0 || strcmp(argv[2], "--verbose") == 0);
+    }
+    else {
+        fprintf(stderr, "Error: No input file provided\n%s\n", getUsage(argv[0]));
+        return -1;
+    }
+    SourceCode src = read_file(filename);
     if (verbose)
         displayCode(src);
     VM* vm = init(src);
