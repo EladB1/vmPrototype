dd2d3e6fef972e3d63d030674bef5b7ede10a49c
EladB1 <eb86@njit.edu>
2024-05-15 15:05:01 -0400

    Added option to get report on memory usage

--- stackVM/src/app.c
+++ stackVM/src/app.c
@@ -11,20 +11,24 @@
 
 char* getUsage(char* prog_name) {
     char* verbose = "\t-v, --verbose:\tDisplay the internal VM state at each execution cycle\n";
+    char* memory = "\t-m, --memory:\tDisplay the amount of memory configured in your configuration file then stop running\n";
     char* help = "\t-h, --help:\tShow this help message\n";
     char* message = "";
-    asprintf(&message, "Usage: %s FILE [OPTIONS]\nOPTIONS:\n%s%s", prog_name, verbose, help);
+    asprintf(&message, "Usage: %s FILE [OPTIONS]\nOPTIONS:\n%s%s%s", prog_name, verbose, memory, help);
     return message;
 }
 
 int main(int argc, char** argv) {
     char filename[256];
     bool verbose;
-
+    bool showMemory = false;
     if (argc >= 2) {
         if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
             printf("%s\n", getUsage(argv[0]));
             return 0;
+        }
+        else if (strcmp(argv[1], "-m") == 0 || strcmp(argv[1], "--memory") == 0) {
+            showMemory = true;
         }
         strncpy(filename, argv[1], strlen(argv[1]) + 1);
         verbose = (argc >= 3) && (strcmp(argv[2], "-v") == 0 || strcmp(argv[2], "--verbose") == 0);
@@ -37,6 +41,12 @@ int main(int argc, char** argv) {
     VMConfig conf = readConfigFile(CONFIG_FILE);
     if (!validateVMConfig(conf, CONFIG_FILE))
         return -1;
+    
+    if (showMemory) {
+        printf("Estimated memory in configuration file '%s':\n---\n", CONFIG_FILE);
+        displayVMConfig(conf);
+        return 0;
+    }
 
     SourceCode* src = read_file(filename);
     if (src == NULL)
